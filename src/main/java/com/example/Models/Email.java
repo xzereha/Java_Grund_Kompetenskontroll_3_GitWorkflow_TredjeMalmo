d54ff87f2c7c7090a842a8e82fedfb99617f7f2e
Oliver Öhrström <xzereha@live.se>
2025-11-03 13:55:45 +0100

    Added some comments about the usage

--- src/main/java/com/example/Models/Email.java
+++ src/main/java/com/example/Models/Email.java
@@ -6,6 +6,20 @@ import java.util.regex.Pattern;
 public class Email {
     private final String value;
 
+    /**
+     * Constructor for Email class
+     * 
+     * This constructor will throw an IllegalArgumentException if the provided email
+     * is not valid.
+     * To ensure the email is valid, use the static isValid method before creating
+     * an instance.
+     * 
+     * @see Email.isValid(String email)
+     * 
+     * @throws IllegalArgumentException if the email is not valid
+     * 
+     * @param email
+     */
     public Email(String email) {
         if (!isValid(email)) {
             throw new IllegalArgumentException("Invalid email");
@@ -17,6 +31,12 @@ public class Email {
         return value;
     }
 
+    /**
+     * Validates the provided email string.
+     * 
+     * @param email the email string to validate
+     * @return true if the email is valid, false otherwise
+     */
     public static boolean isValid(String email) {
         if (email == null || email.isEmpty()) {
             return false;

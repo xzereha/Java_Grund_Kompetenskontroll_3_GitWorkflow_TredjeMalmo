64b53d5e51564ece08b437b4fbb096659fa5fd8d
DevelopingPontus <ipont35@gmail.com>
2025-11-03 14:44:53 +0100

    BookingRepository done

---
+++ src/main/java/com/example/BookingRepository.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import com.example.Models.Booking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookingRepository {
+    private final List<Booking> bookingList = new ArrayList<>();
+
+    public void addBooking(Booking booking) {
+        bookingList.add(booking);
+    }
+
+    public boolean removeBooking(Booking booking) {
+        return bookingList.remove(booking);
+    }
+
+    public List<Booking> getBookingList() {
+        return bookingList;
+    }
+
+    public int size() {
+        return bookingList.size();
+    }
+
+    public boolean isEmpty() {
+        return bookingList.isEmpty();
+    }
+}

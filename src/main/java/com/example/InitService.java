f2e1d6e8adff91608e80bef51289bca78a8e0388
Millaray Soto Chavez <xmsc95@hotmail.se>
2025-11-04 09:21:17 +0100

    InitService

---
+++ src/main/java/com/example/InitService.java
@@ -0,0 +1,75 @@
+package com.example;
+
+import com.example.Models.Booking;
+import com.example.Models.Email;
+import com.example.Models.RegNr;
+import com.example.Models.Vehicle;
+
+import java.time.LocalDate;
+
+public class InitService {
+
+    public static void init(BookingRepository bookingRepository) {
+
+       // TODO: fix price
+        bookingRepository.addBooking(new Booking.Service
+                (new Vehicle(new RegNr("ABC123"), "Model", 2022), new Email("email@address.se"),
+                        LocalDate.of(2025,11,28),2334));
+
+        bookingRepository.addBooking(new Booking.Inspection
+                (new Vehicle(new RegNr("ABC123"), "Model", 2000), new Email("email@address.se"),
+                        LocalDate.of(2025,12,02),4567));
+
+        bookingRepository.addBooking(new Booking.Repair
+                (new Vehicle(new RegNr("ABC123"), "Tesla", 2022), new Email("email@address.se"),
+                        LocalDate.of(2025,12,03),"Doorhandel"));
+
+        bookingRepository.addBooking(new Booking.Service
+                (new Vehicle(new RegNr("ABC123"), "TOYOTA IQ", 2022), new Email("email@address.se"),
+                        LocalDate.of(2025,12,12),4567));
+
+        bookingRepository.addBooking(new Booking.Inspection
+                (new Vehicle(new RegNr("ABC123"), "Model", 2022), new Email("email@address.se"),
+                        LocalDate.of(2025,11,22),9856));
+
+        bookingRepository.addBooking(new Booking.Repair
+                (new Vehicle(new RegNr("ABC123"), "Model", 2022), new Email("email@address.se"),
+                        LocalDate.of(2024,11,26),"carFarts"));
+
+        bookingRepository.addBooking(new Booking.Service
+                (new Vehicle(new RegNr("ABC123"), "Model", 2022), new Email("email@address.se"),
+                        LocalDate.of(2025,11,28),6678));
+
+        bookingRepository.addBooking(new Booking.Inspection
+                (new Vehicle(new RegNr("ABC123"), "Model", 2022), new Email("email@address.se"),
+                        LocalDate.of(2025,11,29),9889));
+
+        bookingRepository.addBooking(new Booking.Repair
+                (new Vehicle(new RegNr("ABC123"), "Model", 2022), new Email("email@address.se"),
+                        LocalDate.of(2024,11,26),"carFarts"));
+
+        bookingRepository.addBooking(new Booking.Service
+                (new Vehicle(new RegNr("ABC123"), "Model", 2022), new Email("email@address.se"),
+                        LocalDate.of(2025,06,26),6778));
+
+        bookingRepository.addBooking(new Booking.Inspection
+                (new Vehicle(new RegNr("ABC123"), "Model", 2022), new Email("email@address.se"),
+                        LocalDate.of(2025,11,26),9898));
+
+        bookingRepository.addBooking(new Booking.Repair
+                (new Vehicle(new RegNr("ABC123"), "Model", 2022), new Email("email@address.se"),
+                        LocalDate.of(2026,03,26),"carFarts"));
+
+        bookingRepository.addBooking(new Booking.Service
+                (new Vehicle(new RegNr("ABC123"), "Model", 2022), new Email("email@address.se"),
+                        LocalDate.of(2026,06,26),9234));
+
+        bookingRepository.addBooking(new Booking.Repair
+                (new Vehicle(new RegNr("ABC123"), "Model", 2022), new Email("email@address.se"),
+                        LocalDate.of(2025,8,26),"carFarts"));
+
+        bookingRepository.addBooking(new Booking.Inspection
+                (new Vehicle(new RegNr("ABC123"), "Model", 2022), new Email("email@address.se"),
+                        LocalDate.of(2026,05,03),9875));
+    }
+}

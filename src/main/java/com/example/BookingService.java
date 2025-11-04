f39366c97a9cf9e0abcd5cbed0f85572a6d3ddd7
Mariozalac716 <zalac.mario1@gmail.com>
2025-11-04 14:47:32 +0100

    New method

--- src/main/java/com/example/BookingService.java
+++ src/main/java/com/example/BookingService.java
@@ -4,11 +4,15 @@ import com.example.Models.Booking;
 import com.example.Models.Email;
 import com.example.Models.RegNr;
 import com.example.Models.Vehicle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 
 public class BookingService {
+    private static final Logger logger = LoggerFactory.getLogger(BookingService.class.getName());
     private final BookingRepository bookingRepository;
+    //private final EmailService emailService;
 
     public BookingService(BookingRepository bookingRepository) {
         this.bookingRepository = bookingRepository;
@@ -21,7 +25,7 @@ public class BookingService {
         var price_ = 1000; // TODO fetch from price list
         var booking = new Booking.Inspection(vehicle, email_, date, price_);
 
-        return bookingRepository.addBooking(booking);
+        return registerBooking(booking);
     }
 
     public boolean bookService(String regNr, String vehicleModel, int yearModel, String email, LocalDate date) {
@@ -31,7 +35,7 @@ public class BookingService {
         var price_ = 1200; // TODO fetch from price list
         var booking = new Booking.Service(vehicle, email_, date, price_);
 
-        return bookingRepository.addBooking(booking);
+        return registerBooking(booking);
     }
 
     public boolean bookRepair(String regNr, String vehicleModel, int yearModel, String email, LocalDate date, String repairDescription) {
@@ -40,8 +44,13 @@ public class BookingService {
         var vehicle = new Vehicle(regNr_, vehicleModel, yearModel);
         var booking = new Booking.Repair(vehicle, email_, date, repairDescription);
 
-        return bookingRepository.addBooking(booking);
+        return registerBooking(booking);
     }
 
+    private boolean registerBooking(Booking booking) {
+        boolean wasAdded = bookingRepository.addBooking(booking);
+
+        return wasAdded;
+    }
 
 }

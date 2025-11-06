292c99adc8f9fac352886675c46886e9873c9b65
Mariozalac716 <zalac.mario1@gmail.com>
2025-11-06 10:40:56 +0100

    small changes

--- src/main/java/com/example/views/BookingListView.java
+++ src/main/java/com/example/views/BookingListView.java
@@ -2,13 +2,10 @@ package com.example.views;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.util.List;
 import java.awt.BorderLayout;
 import javax.swing.*;
 
 import com.example.BookingService;
-import com.example.Models.Booking;
-import com.example.Models.Booking.Status;
 import com.example.sorting.Sort;
 import com.example.sorting.SortByDate;
 import com.example.sorting.SortByID;
@@ -18,12 +15,10 @@ public class BookingListView extends JPanel {
     private final BookingService bookingService;
     private final JPanel listPanel;
     private Sort currentSorting;
-    private Sort currentFilter;
 
     public BookingListView(BookingService bookingService) {
         this.bookingService = bookingService;
-        this.currentSorting = new SortByID(0);
-        this.currentFilter = null;
+        this.currentSorting = new SortByID();
 
         setLayout(new BorderLayout());
         setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
@@ -53,39 +48,24 @@ public class BookingListView extends JPanel {
         buttonPanel.add(addButton);
         add(buttonPanel, BorderLayout.SOUTH);
 
-        var sortingOptions = new String[] {
+        var sortingOptions = new String[]{
                 "Sortera efter ID",
                 "Sortera efter datum",
+                "Sortera efter status"
         };
         JComboBox<String> sortingComboBox = new JComboBox<>(sortingOptions);
-
-        var filterOptions = new String[] {
-                "Inga filter",
-                "Endast pågående",
-                "Endast slutförda",
-        };
-        JComboBox<String> filterComboBox = new JComboBox<>(filterOptions);
 
         sortingComboBox.addActionListener(e -> {
             int selectedIndex = sortingComboBox.getSelectedIndex();
             switch (selectedIndex) {
-                case 0 -> currentSorting = new SortByID(0);
+                case 0 -> currentSorting = new SortByID();
                 case 1 -> currentSorting = new SortByDate(true);
-            }
-            refreshBookings();
-        });
-        filterComboBox.addActionListener(e -> {
-            int selectedIndex = filterComboBox.getSelectedIndex();
-            switch (selectedIndex) {
-                case 0 -> currentFilter = null;
-                case 1 -> currentFilter = new SortByStatus(Status.PENDING);
-                case 2 -> currentFilter = new SortByStatus(Status.COMPLETED);
+                case 2 -> currentSorting = new SortByStatus();
             }
             refreshBookings();
         });
 
         buttonPanel.add(sortingComboBox);
-        buttonPanel.add(filterComboBox);
     }
 
     /**
@@ -94,14 +74,7 @@ public class BookingListView extends JPanel {
     private void refreshBookings() {
         listPanel.removeAll();
 
-        List<Booking> bookings = null;
-        if (currentFilter != null) {
-            bookings = currentFilter.apply(bookingService.listBookings());
-        } else {
-            bookings = bookingService.listBookings();
-        }
-
-        for (var booking : currentSorting.apply(bookings)) {
+        for (var booking : currentSorting.apply(bookingService.listBookings())) {
             BookingView bookingView = new BookingView(booking, bookingService, () -> refreshBookings());
             listPanel.add(bookingView);
             // Add spacing between booking views

cb0468594f53fb3700e624e70a467fb2663703bc
Oliver Öhrström <xzereha@live.se>
2025-11-06 10:11:06 +0100

    Fixed so window spawn in the middle of the screen

--- src/main/java/com/example/views/Menu.java
+++ src/main/java/com/example/views/Menu.java
@@ -11,6 +11,7 @@ public class Menu extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         add(new BookingListView(bookingService));
 
+        setLocationRelativeTo(null);
         setVisible(true);
     }
 }
